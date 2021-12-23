3e4d8529f6c00cc76b24c2bfcde4e1949bde4eda
Sajjad Ranjbar <83742735+phantomf4321@users.noreply.github.com>
2021-12-23 10:08:08 +0330

    Create conversion.cpp

---
+++ void map::make() {/*     s_data = new string* [100];     i_data = new int* [100];     for (int counter1 = 0; counter1 < degree+1; counter1++)     {         s_data[counter1] = new string[degree];     }     for (int counter1 = 0; counter1 < degree + 1; counter1++)     {         i_data[counter1] = new int[degree];     }     */conversion.cpp
@@ -0,0 +1,4 @@
+                cin >> s_data[counter1][counter2];
+                found(s_data[counter1][counter2], counter1, counter2);
+                stringstream geek(s_data[counter1][counter2]);
+                geek >> i_data[counter1][counter2];
